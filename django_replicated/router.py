5f9a9bf6c584ae24c9b051413f88ade55f13d0d8
Dmitry Prokofyev <dmirain@yandex.ru>
2013-08-27 21:01:43 +0400

    Роутер в качестве контекста использует threading.local

--- django_replicated/router.py
+++ django_replicated/router.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import random
+from threading import local
 
 from django.conf import settings
 from django.db.utils import DEFAULT_DB_ALIAS
@@ -10,58 +11,43 @@ from .db_utils import db_is_alive
 class ReplicationRouter(object):
 
     def __init__(self):
-        from .context import context
-        self.context = context
+        self._context = local()
         self.DEFAULT_DB_ALIAS = DEFAULT_DB_ALIAS
         self.DOWNTIME = getattr(settings, 'DATABASE_DOWNTIME', 60)
         self.SLAVES = getattr(settings, 'DATABASE_SLAVES', [DEFAULT_DB_ALIAS])
 
-    def state_stack():
-        def fget(self):
-            return self.context.get('state_stack', [])
+    def _init_context(self):
+        self._context.state_stack = []
+        self._context.chosen = {}
+        self._context.state_change_enabled = True
 
-        def fset(self, value):
-            self.context.state_stack = value
-        return locals()
-    state_stack = property(**state_stack())
+    @property
+    def context(self):
+        if (
+            not hasattr(self._context, 'state_stack') or
+            not hasattr(self._context, 'chosen') or
+            not hasattr(self._context, 'state_change_enabled')
+        ):
+            self._init_context()
 
-    def chosen():
-        def fget(self):
-            return self.context.get('chosen', {})
-
-        def fset(self, value):
-            self.context.chosen = value
-        return locals()
-    chosen = property(**chosen())
-
-    def state_change_enabled():
-        def fget(self):
-            return self.context.get('state_change_enabled', True)
-
-        def fset(self, value):
-            self.context.state_change_enabled = value
-        return locals()
-    state_change_enabled = property(**state_change_enabled())
+        return self._context
 
     def init(self, state):
-        self.state_stack = []
-        self.chosen = {}
-        self.state_change_enabled = True
-
+        self._init_context()
         self.use_state(state)
 
     def is_alive(self, db_name):
         return db_is_alive(db_name, self.DOWNTIME)
 
     def set_state_change(self, enabled):
-        self.state_change_enabled = enabled
+        self.context.state_change_enabled = enabled
 
     def state(self):
         '''
         Current state of routing: 'master' or 'slave'.
         '''
-        if self.state_stack:
-            return self.state_stack[-1]
+        if self.context.state_stack:
+            return self.context.state_stack[-1]
         else:
             return 'master'
 
@@ -70,9 +56,9 @@ class ReplicationRouter(object):
         Switches router into a new state. Requires a paired call
         to 'revert' for reverting to previous state.
         '''
-        if not self.state_change_enabled:
+        if not self.context.state_change_enabled:
             state = self.state()
-        self.state_stack.append(state)
+        self.context.state_stack.append(state)
         return self
 
     def revert(self):
@@ -80,10 +66,10 @@ class ReplicationRouter(object):
         Reverts wrapper state to a previous value after calling
         'use_state'.
         '''
-        self.state_stack.pop()
+        self.context.state_stack.pop()
 
     def db_for_write(self, model, **hints):
-        self.chosen['master'] = self.DEFAULT_DB_ALIAS
+        self.context.chosen['master'] = self.DEFAULT_DB_ALIAS
 
         return self.DEFAULT_DB_ALIAS
 
@@ -91,8 +77,8 @@ class ReplicationRouter(object):
         if self.state() == 'master':
             return self.db_for_write(model, **hints)
 
-        if self.state() in self.chosen:
-            return self.chosen[self.state()]
+        if self.state() in self.context.chosen:
+            return self.context.chosen[self.state()]
 
         slaves = self.SLAVES[:]
         random.shuffle(slaves)
@@ -104,6 +90,6 @@ class ReplicationRouter(object):
         else:
             chosen = self.DEFAULT_DB_ALIAS
 
-        self.chosen[self.state()] = chosen
+        self.context.chosen[self.state()] = chosen
 
         return chosen
