5fe8e67e5d623afbda08e1c16dccbae961b16daf
Alex Koshelev <daevaorn@gmail.com>
2013-10-23 18:59:25 +0400

    1.1 version

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_replicated',
+    version='1.1',
     description='Django DB router for stateful master-slave replication',
     packages=[
         'django_replicated',
