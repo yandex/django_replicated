0d1801ed557218588bd8e75cbc1b94e38fb1412b
Alex Koshelev <daevaorn@gmail.com>
2016-03-16 17:38:39 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_replicated',
-    version='1.2',
+    version='2.0',
     description='Django DB router for stateful master-slave replication',
     packages=[
         'django_replicated',
