be70eea0619ab8b1817b5f876069e4ddef0c298a
Ruslan Alimbaev <justlive@yandex-team.ru>
2016-08-11 15:53:53 +0300

    Added "use_master_simple" and "use_slave_simple" which can be used as decorators on any functions (not only views) and also as a context managers in "with" statements

--- django_replicated/decorators.py
+++ django_replicated/decorators.py
@@ -19,11 +19,42 @@ Usage:
 '''
 from __future__ import unicode_literals
 
+from functools import wraps
+
 from django.utils.decorators import decorator_from_middleware_with_args
 
 from .middleware import ReplicationMiddleware
+from .utils import routers
 
+try:
+    from django.utils.decorators import ContextDecorator
+except ImportError:
+    class ContextDecorator(object):
+        """
+        A base class that enables a context manager to also be used as a decorator.
+        """
+        def __call__(self, func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                with self:
+                    return func(*args, **kwargs)
+            return inner
 
 use_state = decorator_from_middleware_with_args(ReplicationMiddleware)
 use_master = use_state('master')
 use_slave = use_state('slave')
+
+
+class use_state_simple(ContextDecorator):
+    def __init__(self, state):
+        self.state = state
+
+    def __enter__(self):
+        routers.use_state(self.state)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        routers.revert()
+
+
+use_master_simple = use_state_simple('master')
+use_slave_simple = use_state_simple('slave')
