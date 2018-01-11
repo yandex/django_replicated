be3657c11ab24cad3eb0be64306ab5d6e203ffe9
Alex Koshelev <daevaorn@gmail.com>
2018-01-11 15:02:27 +0300

    Version 2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_replicated',
-    version='2.3',
+    version='2.4',
     description='Django DB router for stateful master-slave replication',
     packages=[
         'django_replicated',
